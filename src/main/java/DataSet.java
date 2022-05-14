759f3c64b9c7b57bfd41a4b58a1e8afdddcee858
blindnicknamer <75465704+blindnicknamer@users.noreply.github.com>
2022-05-14 12:18:42 -0400

    Changed DataSet to use DoubleSummaryStatistics

--- src/main/java/DataSet.java
+++ src/main/java/DataSet.java
@@ -1,4 +1,5 @@
 package src.main.java;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,50 +8,101 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.DoubleStream;
+import java.util.DoubleSummaryStatistics;
 
 public class DataSet {
     
     private ArrayList<Location> locations = new ArrayList<Location>();
+
+    private double[] pHs = new double[locations.size()];
+    private double[] nitrites = new double[locations.size()];
+    
+    private DoubleSummaryStatistics pHStats;
+    private DoubleSummaryStatistics nitriteStats;
 
     public DataSet (String pathname) throws FileNotFoundException {
         Scanner lineChopper = new Scanner(new File(pathname));
         while (lineChopper.hasNextLine()) {
             locations.add(new Location(lineChopper.nextLine()));
         }
+
+        for (int i = 0; i < locations.size(); i++) {
+            pHs[i] = locations.get(i).getPH();
+            nitrites[i] = locations.get(i).getNitrite();
+        }
+
+        DoubleSummaryStatistics pHStats = DoubleStream.of(pHs).summaryStatistics();
+        DoubleSummaryStatistics nitriteStats = DoubleStream.of(nitrites).summaryStatistics();
+
     }
 
     public double getPHAverage () {
-        double total = 0;
-        for (Location item : locations) {
-            total += item.getPH();
-        }
-        return total / (double)locations.size();
+        return pHStats.getAverage();
     }
 
     public double getNitriteAverage () {
-        double total = 0;
-        for (Location item : locations) {
-            total += item.getNitrite();
-        }
-        return total / (double)locations.size();
+        return nitriteStats.getAverage();
     }
-
+    
     public double[] getPHRange () {
-        double min = Double.MAX_VALUE;
-        for (Location item : locations) {
-            if (item.getPH() < min) {
-                min = item.getPH();
-            }
-        }
-
-        double max = Double.MIN_VALUE;
-        for (Location item : locations) {
-            if (item.getPH() > min) {
-                max = item.getPH();
-            }
-        }
-        return new double[]{min, max};
+        return new double[] {pHStats.getMin(), pHStats.getMax()};
     }
+
+    public double[] getNitriteRange () {
+        return new double[] {nitriteStats.getMin(), nitriteStats.getMax()};
+    }
+
+    // public double getPHAverage () {
+    //     double total = 0;
+    //     for (Location item : locations) {
+    //         total += item.getPH();
+    //     }
+    //     return total / (double)locations.size();
+    // }
+
+    // public double getNitriteAverage () {
+    //     double total = 0;
+    //     for (Location item : locations) {
+    //         total += item.getNitrite();
+    //     }
+    //     return total / (double)locations.size();
+    // }
+
+    // public double[] getPHRange () {
+    //     double min = Double.MAX_VALUE;
+    //     for (Location item : locations) {
+    //         if (item.getPH() < min) {
+    //             min = item.getPH();
+    //         }
+    //     }
+
+    //     double max = Double.MIN_VALUE;
+    //     for (Location item : locations) {
+    //         if (item.getPH() > min) {
+    //             max = item.getPH();
+    //         }
+    //     }
+    //     return new double[]{min, max};
+    // }
+
+    // public double[] getNitriteRange () {
+    //     double min = Double.MAX_VALUE;
+    //     for (Location item : locations) {
+    //         if (item.getNitrite() < min) {
+    //             min = item.getNitrite();
+    //         }
+    //     }
+
+    //     double max = Double.MIN_VALUE;
+    //     for (Location item : locations) {
+    //         if (item.getNitrite() > min) {
+    //             max = item.getNitrite();
+    //         }
+    //     }
+    //     return new double[]{min, max};
+    // }
+
 
     public static void main (String[] args) throws IOException {
         Scanner dataIn = new Scanner(new File("AllTheDataCorrected.txt"));
--- src/main/java/streamSevenMain.java
+++ src/main/java/streamSevenMain.java
@@ -1,8 +1,9 @@
 package src.main.java;
-public class streamSevenMain{
 
-public static void main(String[] args)
-{
-    System.out.println("Hello World!");
-}
+public class streamSevenMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
 }
