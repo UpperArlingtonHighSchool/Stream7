ec621f1bc5adb7ffe29ddab2f6ed60860aa9a297
blindnicknamer <mousetrap5478@gmail.com>
2022-05-16 13:56:59 -0400

    Added JFreeCharts

--- src/main/java/DataSet.java
+++ src/main/java/DataSet.java
@@ -1,4 +1,5 @@
 package src.main.java;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
--- src/main/java/streamSevenMain.java
+++ src/main/java/streamSevenMain.java
@@ -1,8 +1,12 @@
 package src.main.java;
+
+import org.jfree.chart.*;
+
 public class streamSevenMain{
 
 public static void main(String[] args)
 {
     System.out.println("Hello World!");
+    
 }
 }
