7fdfa0c77b0c6780f87be8a9c3af4dece07c2e54
Craisans <105238622+Craisans@users.noreply.github.com>
2022-05-16 14:03:34 -0400

    Update streamSevenMain.java

--- src/main/java/streamSevenMain.java
+++ src/main/java/streamSevenMain.java
@@ -4,10 +4,12 @@ public class streamSevenMain {
 
     public static void main(String[] args) {
         
+        //This is the part where Collin's program gets the acceptable values off of the web
         Scraper collin = new Scraper();
         System.out.println("Acceptable Nitrite level = "+ collin.getAcceptableNitrite());
         System.out.println("Acceptable pH level = "+ collin.getAcceptablePH());
         
+        //This is the part where Jack's program draws a cool chart of our values with JFreeCharts
         ChartRunner jack = new ChartRunner();
         
         
