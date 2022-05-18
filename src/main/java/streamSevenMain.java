dc174e96549940438b306361bc5308a43fef1efb
blindnicknamer <mousetrap5478@gmail.com>
2022-05-18 13:54:48 -0400

    Passed matrix to chartRunner

--- src/main/java/streamSevenMain.java
+++ src/main/java/streamSevenMain.java
@@ -9,8 +9,11 @@ public class streamSevenMain {
         System.out.println("Acceptable Nitrite level = "+ collin.getAcceptableNitrite());
         System.out.println("Acceptable pH level = "+ collin.getAcceptablePH());
         
+        //This is the part where Nathan's program takes in the data from a text file and puts it into a matrix
+        DataSet pH_vs_nitrite = new DataSet("src/main/resources/data/Nitrite_vs_pH.txt");
+
         //This is the part where Jack's program draws a cool chart of our values with JFreeCharts
-        chartRunner jack = new chartRunner();
+        chartRunner jack = new chartRunner(pH_vs_nitrite.getMatrix());
         
         
         
